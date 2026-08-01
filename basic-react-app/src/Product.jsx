9c1791e412d689e98344599441c22c7ddb946949
Sairaj Patil <sairajpatil035@gmail.com>
2026-08-01 14:23:23 +0530

    fixed product size

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,11 +1,15 @@
 import "./Product.css";
 
 
-function Product () {
+function Product ({title = "none", price, features = []}) {
+const list = features.map((feature) => <li>{feature}</li> );
+
     return ( 
         <div className="Product">
-            <h3>Product Title</h3>
+            <h3>{title}</h3>    
             <h5>Product Descripton</h5>
+            <h5>Price: {price} </h5>
+            <ul>{list}</ul>
         </div>
     );
 }
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -1,13 +1,14 @@
 import Product from "./Product.jsx";
 function ProductTab() {
+  let features_opt = [ "hi-tech","durable"]
       return (
    <>
-  <Product/>
-  <Product/>
-  <Product/>
+  <Product title ='pen' price={40000} features={ features_opt}/>
+  <Product title ='Notebook' price={40000}/>
+  <Product title ='Dairy' price={1} />
   </> 
   
   );
 }
 
-export default ProductTab;
+export default ProductTab; 
