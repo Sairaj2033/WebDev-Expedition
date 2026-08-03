1d90d1738c6763fe6cc4fd378ce57285ded61641
Sairaj Patil <sairajpatil035@gmail.com>
2026-08-03 14:51:20 +0530

    React

--- basic-react-app/src/Price.jsx
+++ basic-react-app/src/Price.jsx
@@ -1,8 +1,28 @@
-export default function price() {
+export default function price({oldPrice, newPrice}) {
+  let OLDstyles = {
+    textDecorationLine:"line-through",
+    color:'grey',
+  };
+   let NEWstyles = {
+    fontWeight: "800",
+    color:'black',
+   }
+
+   let styles = {
+     backgroundColor:'#e0c367',
+     height:'30px',
+     width:'auto',
+     borderBottomLeftRadius:"14px",
+     borderBottomRightRadius:"14px",
+
+   }
+
     return (
-        <div>
-            <p>oldPrice</p>
-            <p>newPrice</p>
+        <div style={styles}>
+            <span style={OLDstyles}>{oldPrice}</span>
+            &nbsp; &nbsp;&nbsp;     
+            
+            <span style={NEWstyles}>{newPrice}</span>
         </div>
     )
 }
--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,15 +1,27 @@
 import "./Product.css";
 import Price from "./Price";
 
-function Product ({title = "none"}) {
+function Product ({title = "none",idx}) {
 //const list = features.map((feature) => <li>{feature}</li> );
 //let isDiscount = price >30000 ? "Discount of 5%" : "";
+
+///PRICE
+let oldPrice = ["1321","200","4303","4831"];
+let newPrice = ["4231","500","5303","9321"];
+let description = [
+    ["1000DPI","5 Programmable Langauge"],
+    ["Good Quality","Good battey"],
+    ["Designed for ipad","iphone"],
+    ["Wireless","Wired"]
+];
+
 
  return ( 
         <div className="Product">
            <h4>{title}</h4>
-            <p>Desciption</p>
-            <Price/>
+            <p>{description[idx][0]}</p>
+            <p>{description[idx][1]}</p>
+            <Price oldPrice={oldPrice[idx]} newPrice={newPrice[idx]}  />
         </div>
     ); 
 }
